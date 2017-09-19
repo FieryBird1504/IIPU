4ba0e1a45cdff343b8b55c92a1dc53b5f0df10e0
Fiery <lex150498@mail.ru>
2017-09-19 22:32:43 +0300

    Добавлена обработка ошибок. Первые попытки работы с файлом базы.

--- iipu.lab1/Program.cs
+++ iipu.lab1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Management;
 using System.Text.RegularExpressions;
@@ -11,29 +12,51 @@ namespace iipu.lab1
         public static void Main(string[] args)
         {
             Console.WriteLine("PCI devices:");
-            using (var searcher = new ManagementObjectSearcher(new ManagementScope(), new SelectQuery("SELECT * from Win32_PnPEntity")))
+
+            try
             {
-                ManagementObjectCollection managers = searcher.Get();
-
-                Regex pciDeviceRegex = new Regex(@"^PCI\\*");
-                string pciDidVidRegexPattern = @"(_)|(&)";
-                const int vidPos = 2;
-                const int didPos = 6;
-                
-                foreach (var manager in managers)
+                using (var searcher = new ManagementObjectSearcher(new ManagementScope(),
+                    new SelectQuery("SELECT * from Win32_PnPEntity")))
                 {
-                    string devId = manager["DeviceID"].ToString();
-                    
-                    if (!pciDeviceRegex.IsMatch(devId))
-                    {
-                        continue;
-                    }
+                    ManagementObjectCollection managers = searcher.Get();
 
-                    var stringParts = Regex.Split(devId, pciDidVidRegexPattern);
-                    Console.WriteLine("VID field: {0}", stringParts[vidPos]);
-                    Console.WriteLine("DID field: {0}", stringParts[didPos]);
-                    Console.WriteLine("------------------------------------");
+                    Regex pciDeviceRegex = new Regex(@"^PCI\\*");
+                    string pciDidVidRegexPattern = @"(_)|(&)";
+                    const int vidPos = 2;
+                    const int didPos = 6;
+                    const string dbFileName = "pci.ids.txt";
+
+                    string vidRegexPattern = @"";
+                    string didRegexPattern = "\t";
+
+                    foreach (var manager in managers)
+                    {
+                        string devId = manager["DeviceID"].ToString();
+
+                        if (!pciDeviceRegex.IsMatch(devId))
+                        {
+                            continue;
+                        }
+
+                        var stringParts = Regex.Split(devId, pciDidVidRegexPattern);
+
+                        using (var fs = File.Open(dbFileName, FileMode.Open))
+                        {
+                            using (var sr = new StreamReader(fs))
+                            {
+                                sr.ReadLine();
+                            }
+                        }
+
+                        Console.WriteLine("VID field: {0}", stringParts[vidPos]);
+                        Console.WriteLine("DID field: {0}", stringParts[didPos]);
+                        Console.WriteLine("------------------------------------");
+                    }
                 }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
             }
         }
     }
